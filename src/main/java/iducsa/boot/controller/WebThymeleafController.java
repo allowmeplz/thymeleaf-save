0707f9f1993dc4b9386e73b501a2c6bfab5db1ef
allowmeplz <wlgnsdl356@gmail.com>
2019-05-15 17:13:31 +0900

    commit 20190515

--- src/main/java/iducsa/boot/controller/WebThymeleafController.java
+++ src/main/java/iducsa/boot/controller/WebThymeleafController.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import idu.cs.exception.ResourceNotFoundException;
 import iducsa.boot.domain.User;
 import iducsa.boot.repositories.UserRepository;
 
 @Controller
 public class WebThymeleafController {
-	@Autowired UserRepository userRepo;
+	@Autowired UserRepository userRepo; // Dependency Injection 
+	// 원래의 사용방법
+	// UserRepository userRepo = new UserRepositoryImpl(); <- interface를 구현화 시킨것(직접해야함)
 	
 	@RequestMapping("/")
 	public String home(Model model) {
@@ -24,19 +28,34 @@ public class WebThymeleafController {
 		model.addAttribute("users", userRepo.findAll());
 		return "index";
 	}
-	@GetMapping("/register")
+	
+	@GetMapping("/register") 
 	public String getRegForm(Model model) {
 		return "form";
 	}
+	
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userlist";
 	}
+	
 	@PostMapping("/users")
 	public String createUser(@Valid @RequestBody User user, Model model) {
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
+	}	
+	
+	@GetMapping("/users/{id}")
+	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId).get(); //.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		// Java Lambda 
+		
+		model.addAttribute("name", user.getName());
+		model.addAttribute("company", user.getCompany());
+		return "user";
+		//return ResponseEntity.ok().body(user);
 	}
 }
--- src/main/java/iducsa/boot/domain/User.java
+++ src/main/java/iducsa/boot/domain/User.java
@@ -12,14 +12,14 @@ import javax.persistence.Table;
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
+	private Long id;
 	private String name;
 	private String company;
 	
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getName() {
